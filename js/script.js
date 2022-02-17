62005badfaa7cd7ebcad901bd06a17b344f5e485
SergioIznaolaDeveloper <sergioiznaola@gmail.com>
2022-02-17 18:21:29 +0100

    quiz first v

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Haber chekeado los 10 para submit
+// si check en correcto valor 1 si no valor 0 añadir a variable contador
+let contador = 0;
+
+document
+  .querySelector(".form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Frena el envío de formulario
+    
+        let correctos = document.querySelectorAll("correcto");
+
+        for (i = 0; i < correctos.length; i++) {
+          if (event.target.correcto.checked) {
+            contador++;
+          }
+        }
+      });
+
+
+//   if(!document.querySelector('input[name="correcto"]:checked')) {
+//     alert('Error, rellena el campo horario');
+//     hasError = true;
+//}
