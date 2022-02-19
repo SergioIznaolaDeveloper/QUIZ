dc445fcc0eccf3e90f8c169b0f7a98d6c68b72ea
SergioIznaolaDeveloper <sergioiznaola@gmail.com>
2022-02-19 12:35:34 +0100

    funcionando

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
-// Haber chekeado los 10 para submit
-// si check en correcto valor 1 si no valor 0 añadir a variable contador
-let contador = 0;
+function evalQuiz(event) {
+  event.preventDefault();
+  let respuestasCorrectas = ["a", "b", "b", "c", "d", "c", "b", "c", "d", "a"];
+  let respuestasUsuario = [];
+  let respuestasPregunta = [];
+  let aciertos = 0;
+  let preguntasRespondidas = [];
 
-document.querySelector(".form").addEventListener("submit", function (event) {
-  event.preventDefault(); // Frena el envío de formulario
-
-  let correctos = document.querySelectorAll("#correcto");
-
-  for (i = 0; i < correctos.length; i++) {
-    if (event.target.correcto[i].checked) {
-      contador++;
+  for (let preguntas = 1; preguntas < 11; preguntas++) {
+    respuestasPregunta = document.getElementsByName("p" + preguntas);
+    for (let j = 0; j < 4; j++) {
+      if (respuestasPregunta[j].checked === true) {
+        respuestasUsuario[
+          preguntas - 1
+        ] = `${preguntas}-${respuestasPregunta[j].value}  `;
+        preguntasRespondidas.push(respuestasPregunta[j].checked);
+        if (
+          respuestasPregunta[j].value === respuestasCorrectas[preguntas - 1]
+        ) {
+          aciertos++;
+        }
+      }
     }
   }
-});
+  if (preguntasRespondidas.length < 10) {
+    console.log(`Has olvidado responder a ${10-preguntasRespondidas.length} preguntas`)
+  } else {
+    console.log(
+      `Resultados: ${respuestasUsuario.join(
+        ""
+      )}, has acertado ${aciertos} preguntas `
+    );
+  }
+}
+
+document.querySelector(".form").addEventListener("submit", evalQuiz);
